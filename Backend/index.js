91a0cfdc73b2798f7602478dcfd5be0f00c650f2
MajidKh230 <khanmajid230@gmail.com>
2024-12-01 23:20:13 +0500

    add frontend and modify backend

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ configDotenv();
 
 const app = express();
 
+// apis
 app.get("/", (req, res) => {
   res.status(200).send({ message: "welcome to Backend" });
 });
@@ -19,10 +20,10 @@ app.get("/posts", (req, res) => {
   });
 });
 
-app.get("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  res.status(200).send({ message: `product ${id}` });
-});
+// app.get("/posts/:id/", (req, res) => {
+//   const id = req.params.id;
+//   res.status(200).send({ message: `product ${id}` });
+// });
 
 
 app.get("/posts/:id/comments", (req, res) => {
@@ -32,6 +33,7 @@ const filterComments = comments.filter((item)=>{
     return item.postId == id
 })
 
+// filtercomments
 if (filterComments.length === 0) {
     return res.status(404).send({
       status: "404",
@@ -40,23 +42,42 @@ if (filterComments.length === 0) {
     });
   }
 
-
 res.status(200).send({
     status:"200",
     message:`product ${id}`,
     comments:filterComments
 })
-//   res.status(200).send({ 
-//     message: `product ${id}`,
-//     data:filterComments,
-//  });
+
 });
 
+app.get("/posts/comments",(req,res)=>{
+     const {id} = req.query;
+const filterComments = comments.filter((item)=>{
+    return item.postId == id
+})
+
+// filtercomments
+if (filterComments.length === 0) {
+    return res.status(404).send({
+      status: "404",
+      message: `No comments found for postId ${id}`,
+      comments: [],
+    });
+  }
+
+res.status(200).send({
+    status:"200",
+    message:`Comments for postId ${id}`,
+    comments:filterComments
+})
+})
 
 
 
 const PORT = process.env.PORT;
 
+
+// server
 app.listen(PORT, () => {
   console.log("servers is sucessfully started");
 });
